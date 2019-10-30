162a67ec3bcd9442c1d341430fb9f0835d8d5e95
zhbhun <zhbhun@gmail.com>
2019-10-30 19:58:48 +0800

    test express async exception

--- framework/express/examples/error/index.js
+++ framework/express/examples/error/index.js
@@ -6,12 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/async', (req, res) => {
+  setTimeout(() => {
+    throw new Error('Unkown!');
+  }, 0);
+});
+
 app.use((req, res, next) => {
   console.log(404);
   res.status(404).send("Sorry can't find that!")
 });
 
 app.use((err, req, res, next) => {
+  console.log(err.status, err.statusCode, err.status_code);
   console.error(500, err.stack)
   res.status(500).send('Something broke!')
 });
