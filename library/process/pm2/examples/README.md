9fcfecc829eccc6819d146464211d5ae8654cea6
zhbhun <zhbhun@gmail.com>
2020-02-05 09:41:16 +0000

    feat: test pm2 hot reload

---
+++ library/process/pm2/examples/README.md
@@ -0,0 +1,6 @@
+## 测试热重载
+
+- pm2 start test.js -i 2 -k 15000
+- pm2 reload test
+- pm2 stop test
+- pm2 delete test
---
+++ library/process/pm2/examples/test.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const id = Date.now(); // 服务 ID 标识
+let isReloading = false; // 是否收到了 SIGINT 信号
+
+log('start - ' + id);
+
+process.on("SIGINT", () => {
+  isReloading = true;
+
+  log("SIGINT - " + id);
+
+  const cleanUp = () => {
+    // ignore
+  };
+
+  server.close(() => {
+    log("server closed - " + id);
+    // Stop after 10 secs
+    setTimeout(() => {
+      cleanUp();
+      process.exit();
+    }, 10000);
+  });
+
+  // Force close server after 15 secs
+  setTimeout(e => {
+    log("server forcing close !!! " + id);
+    cleanUp();
+    process.exit(1);
+  }, 15000);
+});
+
+const app = express();
+
+app.get("/", function(req, res) {
+  setTimeout(function() {
+    res.send(id + ' - ' + String(isReloading) + ' - x');
+  }, 6000);
+});
+
+const server = app.listen(3001, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+});
+
+function log(message) {
+  var filePath = path.resolve(__dirname, "log.txt");
+  var current = fs.readFileSync(filePath, "utf8");
+  fs.writeFileSync(filePath, current + "\n" + (new Date().toISOString()) + ' - ' + message);
+}
