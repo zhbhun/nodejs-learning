7e6c8151c8d4bc12f36480881047518c2aa3a8a0
zhbhun <zhbhun@gmail.com>
2020-02-05 16:33:01 +0000

    test pm2 reload

--- library/process/pm2/examples/test.js
+++ library/process/pm2/examples/test.js
@@ -1,43 +1,38 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
 const id = Date.now(); // 服务 ID 标识
 let isReloading = false; // 是否收到了 SIGINT 信号
+let closeTime = 0;
 
 log('start - ' + id);
 
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
   isReloading = true;
 
-  log("SIGINT - " + id);
+  log('SIGINT - ' + id);
 
   const cleanUp = () => {
     // ignore
   };
 
+  closeTime = Date.now();
+  const begin = Date.now();
+  server.keepAliveTimeout = 1; // 停止 keep alive 机制，避免接受新的网络连接请求，但是不会影响已经存在的网络连接 —— 实际测试 close 之后还是有可能会接收新的网络请求
   server.close(() => {
-    log("server closed - " + id);
-    // Stop after 10 secs
-    setTimeout(() => {
-      cleanUp();
-      process.exit();
-    }, 10000);
-  });
-
-  // Force close server after 15 secs
-  setTimeout(e => {
-    log("server forcing close !!! " + id);
+    log('server closed - ' + id + ' - ' + (Date.now() - begin));
     cleanUp();
-    process.exit(1);
-  }, 15000);
+    process.exit();
+  });
 });
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
+  const time = Date.now(); // closeTime - time 小于 0 的话，表示存在 close 之后的请求连接 —— 要怎样才能保证 close 之后不再处理请求呢？
   setTimeout(function() {
-    res.send(id + ' - ' + String(isReloading) + ' - x');
+    res.send(id + ' - ' + String(isReloading) + ' - ' + (closeTime - time) + ' - y');
   }, 6000);
 });
 
@@ -45,11 +40,14 @@ const server = app.listen(3001, function() {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
 function log(message) {
-  var filePath = path.resolve(__dirname, "test.txt");
-  var current = fs.readFileSync(filePath, "utf8");
-  fs.writeFileSync(filePath, current + "\n" + (new Date().toISOString()) + ' - ' + message);
+  var filePath = path.resolve(__dirname, 'test.txt');
+  var current = fs.readFileSync(filePath, 'utf8');
+  fs.writeFileSync(
+    filePath,
+    current + '\n' + new Date().toISOString() + ' - ' + message
+  );
 }
