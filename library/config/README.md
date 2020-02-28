d37200bc27d4e0eed9238a35e342eefa50c48ad6
zhbhun <zhbhub@gmail.com>
2020-02-28 09:11:15 +0800

    test dotenv boolean

--- library/config/README.md
+++ library/config/README.md
@@ -3,6 +3,7 @@
 ## 解析
 
 - [yaml](https://github.com/eemeli/yaml)
+- [dotenv](https://github.com/motdotla/dotenv)
 
 ## 查找
 
---
+++ library/config/dotenv/test.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+console.log(String(process.env.boolean1), typeof process.env.boolean1);
+console.log(String(process.env.boolean2), typeof process.env.boolean2);
+console.log(String(process.env.boolean3), typeof process.env.boolean3);
