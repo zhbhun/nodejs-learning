b91f8495207a77d6aeb3514fe27c052e8a072081
zhbhun <zhbhun@gmail.com>
2022-10-22 11:48:25 +0800

    docs: screenshot

---
+++ library/utils/screenshot/index.js
@@ -0,0 +1,6 @@
+var screenshot = require("desktop-screenshot");
+
+screenshot("screenshot.png", function (error, complete) {
+  if (error) console.log("Screenshot failed", error);
+  else console.log("Screenshot succeeded");
+});
